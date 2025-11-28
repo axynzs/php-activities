ade684268e4061bbd0318e6732d23630635d1de6
axynzs <yuniezpalma@gmail.com>
2025-11-28 10:29:37 +0800

    Calculator

---
+++ Seatwork1.php
@@ -0,0 +1,23 @@
+<?php
+$x = 10; //variables
+$y = 15;
+
+echo "Num 1 = " . $x . "<br>";
+echo "Num 2 = " . $y . "<br> <br>";
+
+//sum
+$sum = $x + $y;
+echo "The sum of " . $x . " and " . $y . " is " . $sum . ".<br>";
+
+//diff
+$difference = $x - $y;
+echo "The difference of " . $x . " and " . $y . " is " . $difference . ".<br>";
+
+//product
+$product = $x * $y;
+echo "The product of " . $x . " and " . $y . " is " . $product . ".<br>";
+
+//quotient
+$quotient = $x / $y;
+echo "The quotient of " . $x . " and " . $y . " is " . $quotient . ".<br>";
+?>
